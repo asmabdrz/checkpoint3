126c056b61744c0253bc6c442c3a083affd593ff
asmabdrz <82376756+asmabdrz@users.noreply.github.com>
2021-04-26 12:19:46 +0100

    first commit

---
+++ DOM/DOM.js
@@ -0,0 +1,16 @@
+const paragraph = document.querySelector('p');
+const PA = document.getElementsByTagName('p');
+PA[0];
+// const paragraph2 = document.queryslector('#loup');
+const par = document.getElementById(loup);
+const nodeList = document.querySelectorAll('P');
+for (let i = 1; i < nodeList.length; i++) {
+    console.log(nodeList[i].textContent);
+}
+nodeList[3].textContent = "lksjfhsudhqkdojemfhmzdoùmpdl`^peffuz";
+nodeList[4].setAttribute('class', 'asma');
+nodeList[2].style.cssText = "color:bleu; background-color:aqua; font-size:24PX";
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+}
---
+++ funct.js
@@ -0,0 +1,35 @@
+function fullName(x, y) {
+    return x + " " + y;
+
+}
+console.log(fullName("Asma", "Boudrouaz"));
+
+function AddNumber(x, y) {
+    return x + y;
+
+}
+console.log(AddNumber(3, 5));
+
+function AreaRec(x, y) {
+    return x * y;
+
+}
+console.log(AreaRec(3, 5));
+
+function perimetrec(x, y) {
+    return 2 * (x + y);
+
+}
+console.log(perimetrec(3, 5));
+
+function VolumRectogle(x, y, z) {
+    return x * y * z;
+
+}
+console.log(VolumRectogle(3, 5, 5));
+
+function areaCerc(z) {
+    return Math.PI * z * z;
+
+}
+console.log(areaCerc(3, 5, 5));
